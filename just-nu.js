eac2f4e1da24597d54ebc6fa0d7e91e530384c94
Jeremy Karlsson <jeremy.karlsson@tidal.com>
2018-04-04 17:14:30 +0200

    Use title keyword

--- just-nu.js
+++ just-nu.js
@@ -7,7 +7,7 @@ const app = express();
 app.post('/', async (req, res) => {
   try {
     const { count, topics } = await justNu();
-    const headlines = topics.map(({ title, url }) => ({ text: title, title_link: url }));
+    const headlines = topics.map(({ title, url }) => ({ title, title_link: url }));
 
     res.status(200).send({
       'response_type': 'in_channel',
